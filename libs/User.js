c4ad4cadfb0fe282f09bddb47fd5a2408a8ba682
tusharchaudharryy <150551769+tusharchaudharryy@users.noreply.github.com>
2023-11-15 22:08:38 +0530

    Create User.js

---
+++ libs/User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+var userSchema = new mongoose.Schema({
+    username: String, 
+    players: [],
+    currScore : Number,
+    cumScore : Number,
+    bonusProgress: Number,
+    formIndicator: Number
+});
+
+
+module.exports.User = mongoose.model('User', userSchema)
